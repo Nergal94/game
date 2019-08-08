a4d358f0652472b488eb83c4a88c035c8747d79d
Nergal <k.merkulov1994@gmail.com>
2019-08-08 18:40:26 +0300

    add background

--- script/game.js
+++ script/game.js
@@ -1,1 +1,36 @@
-  
+const first = element => document.querySelector(element);
+const all = element => document.querySelectorAll(element);
+
+const canvasBackground = first('#canvas-background');
+
+const canvasWidth = 968;
+const canvasHeight = 808;
+const starSize = 4;
+const maxStar = 3;
+let currentStar = 0;
+
+const makeStar = () => {
+    if (currentStar === maxStar) {
+        return false;
+    }
+    currentStar++;
+    let yLocation = 0;
+    let ctx = canvasBackground.getContext('2d');
+    ctx.fillStyle = '#fff';
+    let xLocation = Math.random() * canvasWidth;
+    ctx.fillRect(xLocation, 0, starSize, starSize);
+
+    const moveStar = () => {
+        if (yLocation >= canvasHeight) {
+            clearInterval(letsFlight);
+            currentStar--;
+        }
+        ctx.clearRect(xLocation - 1, 0, xLocation, canvasHeight);
+        ctx.fillRect(xLocation, yLocation, starSize, starSize);
+        yLocation+=starSize;
+    };
+
+    let letsFlight = setInterval(moveStar, 200);
+};
+
+setInterval(makeStar, 500);
