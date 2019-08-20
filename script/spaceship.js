9b1f70fe8bc33ab057f392d68d7d239bab50dcab
Nergal <k.merkulov1994@gmail.com>
2019-08-20 22:57:13 +0300

    refactoring

--- script/spaceship.js
+++ script/spaceship.js
@@ -4,48 +4,46 @@ let game = canvasGame.getContext('2d');
 
 const drawSpaceShip = ({x,y,width,height,spritePosition,move}) => {
     
-    if(!spaceShip.life) {
+    if(!isGameStarted) {
         return false;
     };
     
     game.clearRect(0, 0, canvasWidth, canvasHeight);
     game.drawImage(shipImg,spritePosition[0],spritePosition[1],spritePosition[2],spritePosition[3],x,y,width,height);
     
-    hitEnemy(torpedos, enemys, killEnemy);
+    hitEnemy(rockets, enemys, killEnemy);
     chekLife();
-    
-    if (torpedos.length > 0) {
-         for(let i = 0; i < torpedos.length; i++) {
-            drawTorpedo(torpedos[i].x, torpedos[i].y, torpedos[i].width, torpedos[i].height);
-          }
+
+    for(let i = 0; i < rockets.length; i++) {
+        drawRockets(rockets[i].x, rockets[i].y, rockets[i].width, rockets[i].height);
     }
+
     
-    if (enemys.length > 0) {
-        for(let i = 0; i < enemys.length; i++) {
-            drawEnemy(enemys[i].x, enemys[i].y, enemys[i].width, enemys[i].height);
-        }
+ 
+    for(let i = 0; i < enemys.length; i++) {
+        drawEnemy(enemys[i].x, enemys[i].y, enemys[i].width, enemys[i].height);
     }
+  
     
-    if (enemyTorpedos.length > 0) {
-        for(let i = 0; i < enemyTorpedos.length; i++) {
-            drawEnemyTorpedos(enemyTorpedos[i].x, enemyTorpedos[i].y, enemyTorpedos[i].width, enemyTorpedos[i].height);
-        }
+    for(let i = 0; i < enemyRockets.length; i++) {
+        drawEnemyRockets(enemyRockets[i].x, enemyRockets[i].y, enemyRockets[i].width, enemyRockets[i].height);
     }
+ 
     
     move === 'right' && drawSpaceShipMoveRight();
     move === 'left' && drawSpaceShipMoveLeft();
 };
 
-const drawTorpedo = (dx,dy,width,height) => {
-    game.drawImage(torpedoImg, dx, dy, width, height);  
+const drawRockets = (dx,dy,width,height) => {
+    game.drawImage(rocketsImg, dx, dy, width, height);  
 };
 
 const drawEnemy = (dx,dy,width,height) => {
     game.drawImage(enemyImg, dx, dy, width, height); 
 };
 
-const drawEnemyTorpedos = (dx,dy,width,height) => {
-    game.drawImage(enemyTorpedoImg, dx, dy, width, height);
+const drawEnemyRockets = (dx,dy,width,height) => {
+    game.drawImage(enemyRocketImg, dx, dy, width, height);
 };
 
 
@@ -65,29 +63,29 @@ const drawSpaceShipMoveLeft = () => {
 
 const chekLife = () => {
     const indexDeadEnemy = enemys.findIndex(curentValue => curentValue.life === false);
-    const indexDeadTorpedo = torpedos.findIndex(curentValue => curentValue.life === false);
+    const indexDeadRocket = rockets.findIndex(curentValue => curentValue.life === false);
     
-    if ((indexDeadEnemy > -1) && (indexDeadTorpedo) > -1 ) {
+    if ((indexDeadEnemy > -1) && (indexDeadRocket) > -1 ) {
         enemys.splice(indexDeadEnemy,1);
-        torpedos.splice(indexDeadTorpedo,1);
+        rockets.splice(indexDeadRocket,1);
     }
 };
 
 
 const createEnemy = () => {
-    if(enemys.length === maxEnemys || !spaceShip.life) {
+    if(enemys.length === maxEnemys || !isGameStarted) {
         return false;
     }
     
-    let enemy = new Object;
-    enemy['x'] = Math.random() * canvasWidth;
-    enemy['y'] = 0;
-    enemy['width'] = 30;
-    enemy['height'] = 30;
-    enemy['life'] = true;
-    enemy['directionY'] = 'down';
-    enemy.x > canvasWidth/2 && (enemy['direction'] = 'left');
-    enemy.x < canvasWidth/2 && (enemy['direction'] = 'right');
+    let enemy = new Object();
+    enemy.x = Math.random() * canvasWidth;
+    enemy.y = 0;
+    enemy.width = 30;
+    enemy.height = 30;
+    enemy.life = true;
+    enemy.direction = 'down';
+    enemy.x > canvasWidth/2 && (enemy.direction = 'left');
+    enemy.x < canvasWidth/2 && (enemy.direction = 'right');
     
     enemys.push(enemy);
     
@@ -98,64 +96,64 @@ const createEnemy = () => {
             return false;
         }
         
-        enemy = moveEnemyXY(enemy, canvasWidth, LINE_OF_ATTACK);
+        moveEnemyXY(enemy, canvasWidth, LINE_OF_ATTACK);
         
     }, ENEMY_SPEED);
     
-    setInterval(function(){
-        if(!enemy.life) {
-            return false;
-        }
-        
-        let enemyTorpedo = new Object;
-        enemyTorpedo['x'] = enemy.x;
-        enemyTorpedo['y'] = enemy.y;
-        enemyTorpedo['life'] = true;
-        enemyTorpedo['width'] = 10;
-        enemyTorpedo['height'] = 20;
-        
-        enemyTorpedos.push(enemyTorpedo);
-        
-        const moveEnemyTorpedo = setInterval(function(){
-            if(enemyTorpedo.y > canvasHeight) {
-                clearInterval(moveEnemyTorpedo);
-                enemyTorpedo.life = false;
-                return false;
-            }
-            
-            enemyTorpedo.x = moveEnemyTorpedoX(enemyTorpedo.x, (spaceShip.x + spaceShip.width/2));
-            enemyTorpedo.y+=3;
-            
-        }, ENEMY_TORPEDO_SPEED);
-        
-    },FIRE_ENEMY_DELAY);
+//    setInterval(function(){
+//        if(!enemy.life) {
+//            return false;
+//        }
+//        
+//        let enemyRocket = new Object;
+//        enemyRocket['x'] = enemy.x;
+//        enemyRocket['y'] = enemy.y;
+//        enemyRocket['life'] = true;
+//        enemyRocket['width'] = 10;
+//        enemyRocket['height'] = 20;
+//        
+//        enemyRockets.push(enemyRocket);
+//        
+//        const moveEnemyRocket = setInterval(function(){
+//            if(enemyRocket.y > canvasHeight) {
+//                clearInterval(moveEnemyRocket);
+//                enemyRocket.life = false;
+//                return false;
+//            }
+//            
+//            enemyRocket.x = moveEnemyRocketX(enemyRocket.x, (spaceShip.x + spaceShip.width/2));
+//            enemyRocket.y+=3;
+//            
+//        }, ENEMY_ROCKETS_SPEED);
+//        
+//    },FIRE_ENEMY_DELAY);
 };
 
 const fire = () => {
     fireAudio.play();
     
-    let torpedo = new Object;
-    torpedo['x'] = spaceShip.x + (spaceShip.width/2); 
-    torpedo['y'] = spaceShip.y;
-    torpedo['width'] = 5;
-    torpedo['height'] = 10;
-    torpedo['life'] = true;
+    let rocket = new Object;
+    rocket['x'] = spaceShip.x + (spaceShip.width/2); 
+    rocket['y'] = spaceShip.y;
+    rocket['width'] = 5;
+    rocket['height'] = 10;
+    rocket['life'] = true;
     
-    torpedos.push(torpedo);
+    rockets.push(rocket);
     
     const moveTorpedo = setInterval(function(){
-        if(!torpedo.life) {
+        if(!rocket.life) {
             clearInterval(moveTorpedo);
             return false;
         }
         
-        if(torpedo.y <= 0) {
+        if(rocket.y <= 0) {
             clearInterval(moveTorpedo);
-            torpedos.splice(0,1);
+            rockets.splice(0,1);
         }
         
-        torpedo.y-=10;
-    },TORPEDO_SPEED);
+        rocket.y-=10;
+    },ROCKETS_SPEED);
 };
 
 
@@ -190,9 +188,7 @@ window.addEventListener('keydown', controlSpaceShip);
 window.addEventListener('keyup', keyUp);
 
 
-setTimeout(function(){
-    setInterval(createEnemy, 500);
-},5000);
-
+setInterval(createEnemy, CREATE_ENEMY_DELAY);
+setInterval(() => fireEnemy(enemys), FIRE_ENEMY_DELAY);
 
 flight();
--- script/utils.js
+++ script/utils.js
@@ -5,6 +5,7 @@ const startButton = first('#new-game');
 
 const canvasWidth = 960;
 const canvasHeight = 750;
+let isGameStarted = false;
 
 let spaceShip = {
     life: false,
@@ -22,31 +23,32 @@ const KEY_CODES = {
     SPACE: 32
 };
 
-const TORPEDO_SPEED = 25;
+const ROCKETS_SPEED = 25;
 const ENEMY_SPEED = 50;
-const LINE_OF_ATTACK = 500;
-const FIRE_ENEMY_DELAY = 3500;
-const ENEMY_TORPEDO_SPEED = 25;
+const LINE_OF_ATTACK = 400;
+const FIRE_ENEMY_DELAY = 3000;
+const ENEMY_ROCKETS_SPEED = 25;
+const CREATE_ENEMY_DELAY = 500;
 
-let torpedos = [];
+let rockets = [];
 
-let enemyTorpedos = [];
+let enemyRockets = [];
 
 let enemys = [];
 
 let maxEnemys = 5;
 
-const shipImg = new Image;
+const shipImg = new Image();
 shipImg.src = "img/spaceship.png";
 
-const torpedoImg = new Image;
-torpedoImg.src = 'img/torpedo.png';
+const rocketsImg = new Image();
+rocketsImg.src = 'img/torpedo.png';
 
-const enemyImg = new Image;
+const enemyImg = new Image();
 enemyImg.src = 'img/enemy.png';
 
-const enemyTorpedoImg = new Image;
-enemyTorpedoImg.src = 'img/enemy-torpedo.png';
+const enemyRocketImg = new Image();
+enemyRocketImg.src = 'img/enemy-torpedo.png';
 
 const fireAudio = new Audio('audio/fire.mp3');
 const killEnemy = new Audio('audio/Kill_Enemy_Sound_Effect.mp3');
@@ -63,7 +65,7 @@ const hitEnemy = (bullets,ships, music) => {
             const checkYHigh = ships[j].y - bullets[i].y < ships[j].height;
             const chekYLow = ships[j].y - bullets[i].y > -ships[j].height;
             
-            const chekXRight = ships[j].x - bullets[i].x < (ships[j].width);
+            const chekXRight = ships[j].x - bullets[i].x < (bullets[i].width);
             const chekXLeft = ships[j].x - bullets[i].x > -(ships[j].width + bullets[i].width);
     
             if(checkYHigh && chekYLow) {
@@ -78,45 +80,83 @@ const hitEnemy = (bullets,ships, music) => {
     }
 };
 
-const moveEnemyXY = (enemy,maxX,maxY) => {
-    if(enemy.directionX === 'left' && enemy.x > enemy.width) {
-        enemy.x-=3;
-    } else {
-        enemy.directionX = 'right';
+const moveEnemyX = (enemy, max, dx = 3) => {
+    const isCanMoveLeft = enemy.directionX === 'left' && enemy.x > enemy.width;
+    const isCanMoveRight = !isCanMoveLeft && enemy.x < (max - enemy.width);
+    const isCanMove = isCanMoveLeft || isCanMoveRight;
+
+    if (isCanMove) {
+        dx = isCanMoveLeft ? -dx : dx;
+        enemy.x += dx;
     }
-    
-    if(enemy.directionX === 'right' && enemy.x < (maxX - enemy.width)) {
-        enemy.x+=3;
-    } else {
-        enemy.directionX = 'left';
-    }
-    
-    if(enemy.directionY === 'down' && enemy.y < maxY ) {
-        enemy.y+=3;
-    } else {
-        enemy.directionY = 'up';
-    }
-    
-    if(enemy.directionY === 'up' && enemy.y > enemy.height ) {
-        enemy.y-=3;
-    } else {
-        enemy.directionY = 'down';
-    }
-    
-    return enemy;
+
+    enemy.directionX = isCanMoveRight ? 'right' : 'left';
 };
 
-const moveEnemyTorpedoX = (x, targetX) => {
-    if(x < targetX) {
-        x+=0.5;
-    } else {
-        x-=0.5;
+
+const moveEnemyY = (enemy, max, dx = 3) => {
+    const isCanMoveDown = enemy.directionY === 'down' && enemy.y < max;
+    const isCanMoveUp = !isCanMoveDown && enemy.y > enemy.height ;
+    const isCanMove = isCanMoveDown || isCanMoveUp;
+
+    if (isCanMove) {
+        dx = isCanMoveUp ? -dx : dx;
+        enemy.y += dx;
     }
-    return x;
+
+    enemy.directionY = isCanMoveUp ? 'up' : 'down';
+};
+
+const moveEnemyXY = (enemy, maxX, maxY) => {
+    moveEnemyX(enemy, maxX);
+    moveEnemyY(enemy, maxY);
+};
+
+const moveEnemyRocketX = (rocket, targetX) => {
+    if(rocket.x < targetX) {
+        rocket.x+=1;
+    } else {
+        rocket.x-=1;
+    }
+};
+
+const fireEnemy = allEnemys => {
+    let indexOfFireEnemy = Math.ceil((Math.random() * allEnemys.length));
+    
+    if(indexOfFireEnemy === allEnemys.length) {
+        indexOfFireEnemy--;
+    }
+    
+    if(!isGameStarted || allEnemys.length === 0) {
+        return false;
+    }
+        
+    let enemyRocket = new Object();
+    
+    enemyRocket.x = allEnemys[indexOfFireEnemy].x;
+    enemyRocket.y = allEnemys[indexOfFireEnemy].y;
+    enemyRocket.life = true;
+    enemyRocket.width = 10;
+    enemyRocket.height = 20;
+        
+    enemyRockets.push(enemyRocket);
+    
+    const moveEnemyRocket = setInterval(function(){
+        if(enemyRocket.y > canvasHeight) {
+            clearInterval(moveEnemyRocket);
+            enemyRocket.life = false;
+            enemyRockets.splice(0,1);
+            return false;
+        }
+            
+        moveEnemyRocketX(enemyRocket, (spaceShip.x + spaceShip.width/2));
+        enemyRocket.y+=3;
+            
+    }, ENEMY_ROCKETS_SPEED);
 };
 
 const startNewGame = () => {
-    spaceShip.life = true;
+    isGameStarted = true;
     startButton.style.display = 'none';
 };
 
